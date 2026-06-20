794eb287961ff0c8389064cbbdbefc24e08090ab
iszyeffiong <israel.iszy2336@gmail.com>
2026-06-20 14:27:21 +0100

    Fix Tailwind CSS Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     TanStackRouterVite(),
     react(),
     tsconfigPaths(),
